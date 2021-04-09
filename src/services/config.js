10a66d3599591fb999351162b0b547988f78b17f
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-04-09 18:04:22 -0300

    fix: fixing of the pokeapi BASEURL

--- src/services/config.js
+++ src/services/config.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export const http = axios.create({ 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     baseURL: 'https://pokeapi.co/api/v2/'
 =======
     baseURL: 'https://pokeapi.co/api/v2/pokemon/'
@@ -10,4 +11,7 @@ export const http = axios.create({
 =======
     baseURL: 'https://pokeapi.co/api/v2/pokemon/'
 >>>>>>> 12d7718... add: creation of the config.js file that will export a new axios instance
+=======
+    baseURL: 'https://pokeapi.co/api/v2/'
+>>>>>>> 92b2521... fix: fixing of the pokeapi BASEURL
  })
