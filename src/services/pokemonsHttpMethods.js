f930fb173e1c91f306108407255e5f9a6aee54c8
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-04-09 18:06:53 -0300

    add: creation of the fetchPokemons method that will get the api data

--- src/services/pokemonsHttpMethods.js
+++ src/services/pokemonsHttpMethods.js
@@ -2,6 +2,10 @@ import { http } from './config'
 
 export default {
     fetchPokemons:()=> {
+<<<<<<< HEAD
         return http.get('pokemon/?limit=5')
+=======
+        return http.get('pokemon')
+>>>>>>> 3577e02... add: creation of the fetchPokemons method that will get the api data
     }
 }
