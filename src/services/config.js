e028933ac921d45a9c132284cf7a8c67f0c4a103
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-04-09 17:46:42 -0300

    add: creation of the config.js file that will export a new axios instance

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
 export const http = axios.create({ 
+<<<<<<< HEAD
     baseURL: 'https://pokeapi.co/api/v2/'
+=======
+    baseURL: 'https://pokeapi.co/api/v2/pokemon/'
+>>>>>>> 12d7718... add: creation of the config.js file that will export a new axios instance
  })
