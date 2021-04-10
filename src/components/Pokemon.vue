08b1bf44f84a9020210abc89c764227d684dc749
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-04-10 01:41:43 -0300

    add: feeding the Pokemon.vue component by props

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -9,12 +9,18 @@
 =======
       <div class="vertical-alignment">
           <div class="img-wrapper">
+<<<<<<< HEAD
               <img src="" alt="" class="pokemon-thumb-img">
 >>>>>>> 9907ebc... add: creation of the Pokemon.vue component template
+=======
+              
+              <img :src="thumbImg" alt="" class="pokemon-thumb-img">
+>>>>>>> b5e0dfa... add: feeding the Pokemon.vue component by props
           </div>
 
           <div class="syde-by-syde-alignment">
               <p class="pokemon-name">
+<<<<<<< HEAD
 <<<<<<< HEAD
                   {{ pokemonName }}
               </p>
@@ -28,9 +34,12 @@
 >>>>>>> e5bc2a6... add: creation of the Pokemon.vue component, component which is responsible for displaying each pokemon
 =======
 
+=======
+                  {{ pokemonName }}
+>>>>>>> b5e0dfa... add: feeding the Pokemon.vue component by props
               </p>
               <p class="pokemon-id">
-
+                  {{ pokemonId }}
               </p>
           </div>
       </div>
@@ -41,10 +50,14 @@
 <script>
 export default {
 <<<<<<< HEAD
+<<<<<<< HEAD
     props: ['thumbImg', 'pokemonName', 'pokemonId']    
 =======
   
 >>>>>>> e5bc2a6... add: creation of the Pokemon.vue component, component which is responsible for displaying each pokemon
+=======
+    props: ['thumbImg', 'pokemonName', 'pokemonId']    
+>>>>>>> b5e0dfa... add: feeding the Pokemon.vue component by props
 }
 </script>
 
