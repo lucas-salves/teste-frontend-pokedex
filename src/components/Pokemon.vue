9907ebcd72fd9d7372ae7b2279cf105da3c20bde
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-04-10 00:29:29 -0300

    add: creation of the Pokemon.vue component template

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="pokemon-item">
-    
+      <div class="vertical-alignment">
+          <div class="img-wrapper">
+              <img src="" alt="" class="pokemon-thumb-img">
+          </div>
+
+          <div class="syde-by-syde-alignment">
+              <p class="pokemon-name">
+
+              </p>
+              <p class="pokemon-id">
+
+              </p>
+          </div>
+      </div>
   </div>
 </template>
 
