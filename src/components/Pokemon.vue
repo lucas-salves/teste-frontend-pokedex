e5bc2a69735d782af5182e703c2fceeb3f71af67
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-04-10 00:23:33 -0300

    add: creation of the Pokemon.vue component, component which is responsible for displaying each pokemon

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pokemon-item">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
