58529ef4c1ed9ce26d8086617d216ff7dcc5b7b5
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-04-10 01:47:55 -0300

    test: limit the number of rows of the API response to 5 to make testing easier

--- src/services/pokemonsHttpMethods.js
+++ src/services/pokemonsHttpMethods.js
@@ -4,6 +4,7 @@ export default {
     fetchPokemons:()=> {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         return http.get('pokemon/?limit=5')
 =======
         return http.get('pokemon')
@@ -11,5 +12,8 @@ export default {
 =======
         return http.get('pokemon/?limit=151')
 >>>>>>> 3dd4199... add: adding the limit parameter to control the number of rows returned, so that the number of lines returned is 151
+=======
+        return http.get('pokemon/?limit=5')
+>>>>>>> 1e29da3... test: limit the number of rows of the API response to 5 to make testing easier
     }
 }
