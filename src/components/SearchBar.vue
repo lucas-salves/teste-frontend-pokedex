7401508fbd0dcdacc931623b9c1d90c3f6cf0bdf
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-04-10 01:46:33 -0300

    add: creation of the component SearchBar.vue that will allow users filter pokemons by a text input

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search-bar-container">
+    
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
