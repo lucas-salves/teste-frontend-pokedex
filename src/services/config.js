a6dc4ae35727ac1abd91076a8ad5f40304819142
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-04-10 02:56:36 -0300

    fix: fixing some merge conflicts

--- src/services/config.js
+++ src/services/config.js
@@ -1,17 +1,6 @@
 import axios from 'axios'
 
 export const http = axios.create({ 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     baseURL: 'https://pokeapi.co/api/v2/'
-=======
-    baseURL: 'https://pokeapi.co/api/v2/pokemon/'
->>>>>>> 12d7718... add: creation of the config.js file that will export a new axios instance
-=======
-    baseURL: 'https://pokeapi.co/api/v2/pokemon/'
->>>>>>> 12d7718... add: creation of the config.js file that will export a new axios instance
-=======
-    baseURL: 'https://pokeapi.co/api/v2/'
->>>>>>> 92b2521... fix: fixing of the pokeapi BASEURL
  })
