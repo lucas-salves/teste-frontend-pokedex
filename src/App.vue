8484260fb9f15b58ef780c257c24ec23a49dd2b8
Lucas Sousa Alves <lucas.salves@terra.com.br>
2021-04-10 15:57:56 -0300

    add: searchbar implementation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
 
-    <basesearchbar></basesearchbar>
-    <pokemon v-for="pokemon in pokemonsList"
-             :key="pokemon.id" 
+    <basesearchbar
+    v-on:searchTextFromSearchBar="onSearchBarKeyup"
+    ></basesearchbar>
+    <pokemon v-for="(pokemon, index) in pokemonsList"
+             :key="index" 
              :pokemon-name="pokemon.name"
              :pokemonId="pokemon.id"
              :thumbImg="pokemon.sprites['front_default']" >
@@ -31,35 +33,82 @@ import axios from 'axios'
 
 
 export default {
+  data(){
+    return {
+      pokemonsList: [],
+      pokemonsUrl: [],
+      pokemonsFilter: [],
+      searchBarMessage: ''
+    }
+  },
   components: {
     Pokemon,
     basesearchbar
   },
-
-  data(){
-    return {
-      pokemonsList: [],
-      pokemonsUrl: []
-      
-    }
-  },
-
   mounted(){
-    PokemonApi.fetchPokemons().then(response => {      
-      this.pokemonsUrl = response.data.results.map( pokemon => pokemon.url)
-      this.pokemonsUrl.forEach((pokemonUrl) => {   
-        axios.get(pokemonUrl)
-        .then(
-          response => {
-            return this.pokemonsList.push(response.data)
-          }) ; 
+    // PokemonApi.fetchPokemons().then(response => {      
+    //   this.pokemonsUrl = response.data.results.map( pokemon => pokemon.url)
+    //   this.pokemonsUrl.forEach((pokemonUrl) => {   
+    //     axios.get(pokemonUrl)
+    //     .then(
+    //       response => {
+    //         return this.pokemonsList.push(response.data)
+    //       }) ; 
           
-      })
+    //   })
       
-    })
-    //  console.log(this.pokemonsObjectList)
+    // })
+    this.fetchPokemonsBy('url')
+  },
+  methods: {
+    onSearchBarKeyup(value){
+      this.searchBarMessage = value;
+      let result;
+      if(value == ''){
+        // let uniquePokemonList = new Set(this.pokemonsList)
+       // let uniquePokemonList = new Set(this.pokemonsList)
+        result=''
+        console.log(this.pokemonsList)
+        this.fetchPokemonsBy('url')
+      }
+      
+      this.pokemonsList.forEach((pokemon) => {  
+          
+        if(pokemon.name.includes(value)){
+          
+           result = this.pokemonsList.filter( pokemon => pokemon.name.includes( value) )
+           
+        }else{
+          console.log('nenhum resultado encontrado')
+          
+        }
+      })
+
+       //  result = this.pokemonsList.filter( pokemon => pokemon.name == value)
+        // console.log(value)
+        this.pokemonsList = result
+      // console.log(this.pokemonsList)
+      // this.pokemonsList.filter(function(el){
+      //   return el.name
+      // })
+      // return result
+    },
+    fetchPokemonsBy(filter){
+      PokemonApi.fetchPokemons().then(response => {        
+          this.pokemonsFilter = response.data.results.map(pokemon => pokemon[filter])
+        this.pokemonsFilter.forEach((pokemon) => {
+          axios.get(pokemon).then(response => {
+                
+               this.pokemonsList.push(response.data)
+          })
+        })
+      })
+
+    }
   }
-}
+
+ }
+
 </script>
 
 <style lang="scss">
--- src/components/BaseSearchBar.vue
+++ src/components/BaseSearchBar.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="search-bar-container">
     <form>
-      <input type="text">
+      <input type="text" v-model="searchText" v-on:keyup="emitSearchText">
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'basesearchbar'
+  name: 'basesearchbar',
+  data(){
+    return {
+      searchText: ''
+    }
+  },
+  methods: {
+    emitSearchText(){      
+      this.$emit('searchTextFromSearchBar', this.searchText)
+    }
+  }
 }
 </script>
 
